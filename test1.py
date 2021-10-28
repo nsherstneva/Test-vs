d408365b93257e1fc1a434a21312ec86f9cff485
nsherstneva <n.sherstneva@gmail.com>
2021-10-28 16:50:47 +0200

    New Commit

--- test1.py
+++ test1.py
@@ -4,26 +4,11 @@ import re
 import numpy as np
 import pandas as pd
 
-ss = 'Hello'
+ss = 'Hello !!!'
 
 s1 = re.search('ll*', ss).group()
 
 print('{} - {}'.format(ss, s1))
 
-people = {
-    "first": ["Corey"],
-    "last": ["Schafer"],
-    "email": ["CoreyMSchafer@gmail.com"]
-}
-df = pd.DataFrame(people)
 
-print(df)
-
-
-def numers():
-    """[function]
-    """
-
-    for i in range(0, 10):
-        print(i)
 
